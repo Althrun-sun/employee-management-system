cb0bd10c0432795250cdeed638730a191aa66922
althrun <althrunsun@gmail.com>
2023-02-08 21:13:06 -0800

    CRUD api achieved

--- employee-system-api/src/main/java/com/althrun/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/althrun/employee/controller/EmployeeController.java
@@ -3,7 +3,12 @@ package com.althrun.employee.controller;
 import com.althrun.employee.model.Employee;
 import com.althrun.employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -17,5 +22,30 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeService.createEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+        boolean deleted =false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee =null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee employee) {
+        employee = employeeService.updateEmployee(id,employee);
+        return ResponseEntity.ok(employee);
     }
 }
